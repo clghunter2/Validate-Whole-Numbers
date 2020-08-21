aa845f27d6bcb175f4a0667ecd30c0e086bc9f11
Gavin Hunter <clghunter2@glow.sch.uk>
2020-08-21 09:42:29 +0000

    it's not text

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-number = float(input("Enter number"))
-while number < 4 or number > 10 or not number.is_integer():
-    print('Error. Youre daft!')
-    number = float(input("Enter number"))
+# Validate whole numbers
+# (not decimal and not text)
 
-number = int(number)
+
+number = False
+
+while not number:
+    string_in = input("Please enter a whole number")
+    number = True
+    for letter in string_in:
+        if letter >= '0' and letter <= '9' or letter == '.':
+            print (letter)
+        else:
+            number = False
